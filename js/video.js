c7f8b66b4389f3a6bb2e4db878259e2b2087736b
allislee <123308102+allislee@users.noreply.github.com>
2023-11-05 23:50:47 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -6,6 +6,18 @@ window.addEventListener("load", function() {
 	this.document.querySelector("video").autoplay = false;
 	var video = document.getElementById("player1");
 
+	this.document.querySelector("#play").addEventListener("click", function() {
+		console.log("Play Video");
+		document.getElementById("player1").play();
+		document.getElementById("player1").volume = document.getElementById("slider").value / 100;
+		document.getElementById("volume").innerHTML = document.getElementById("slider").value;
+	});
+
+	document.querySelector("#pause").addEventListener("click", function() {
+		console.log("Pause video");
+		document.getElementById("player1").pause();
+	});
+
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
