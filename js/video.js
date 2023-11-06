ff9a45d95deb475a5d6de2e2d0f7f8507b2bb80a
allislee <123308102+allislee@users.noreply.github.com>
2023-11-05 23:27:51 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -17,12 +17,45 @@ window.addEventListener("load", function() {
 		console.log("Pause video");
 		document.getElementById("player1").pause();
 	});
-
+	
 	document.querySelector("#slower").addEventListener("click", function() {
 		document.getElementById("player1").playbackRate = document.getElementById("player1").playbackRate * 0.9;
 		console.log('New speed is ' + document.getElementById("player1").playbackRate);
 	});
+	
+	document.querySelector("#faster").addEventListener("click", function() {
+		document.getElementById("player1").playbackRate = document.getElementById("player1").playbackRate * 1.1;
+		console.log('New speed is ' + document.getElementById("player1").playbackRate);
+	});
+	
+	document.querySelector("#skip").addEventListener("click", function() {
+		document.getElementById("player1").currentTime = document.getElementById("player1").currentTime + 10;
+		console.log('Current location is' + document.getElementById("player1").currentTime);
+	});
+	
+	document.querySelector("#mute").addEventListener("click", function() {
+		if(document.getElementById("player1").muted == true) {
+			document.getElementById("player1").muted = false;
+			document.querySelector("#mute").innerHTML = "Mute";
+		}
+		else {
+			document.getElementById("player1").muted = true;
+			document.querySelector("#mute").innerHTML = "Unmute";
+		}
+	});
 
+	this.document.querySelector("#slider").addEventListener("change", function () {
+		video.volume = document.getElementById("slider").value / 100;
+		document.getElementById("volume").innerHTML = document.getElementById("slider").value;
+	});
+
+	this.document.querySelector("#vintage").addEventListener("click", function() {
+		video.classList.add("oldSchool");
+	});
+
+	this.document.querySelector("#orig").addEventListener("click", function() {
+		video.classList.remove("oldSchool");
+	});
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
