10c976f449cf71611c2254acbd624467548796f1
allislee <123308102+allislee@users.noreply.github.com>
2023-11-05 23:18:25 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,30 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
+	this.document.querySelector("video").loop = false;
+	this.document.querySelector("video").autoplay = false;
+	var video = document.getElementById("player1");
+
+	this.document.querySelector("#play").addEventListener("click", function() {
+		console.log("Play Video");
+		document.getElementById("player1").play();
+		document.getElementById("player1").volume = document.getElementById("slider").value / 100;
+		document.getElementById("volume").innerHTML = document.getElementById("slider").value;
+	});
+
+	document.querySelector("#pause").addEventListener("click", function() {
+		console.log("Pause video");
+		document.getElementById("player1").pause();
+	});
+
+	document.querySelector("#slower").addEventListener("click", function() {
+		document.getElementById("player1").playbackRate = document.getElementById("player1").playbackRate * 0.9;
+		console.log('New speed is ' + document.getElementById("player1").playbackRate);
+	});
 
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
-
